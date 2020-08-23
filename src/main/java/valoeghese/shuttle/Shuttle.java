f23d9f7e603e3f566f530567f4907e635cb2e2b2
valoeghese <valoeghesemods@gmail.com>
2020-08-24 09:10:36 +1200

    Let's start this!

---
+++ src/main/java/valoeghese/shuttle/Shuttle.java
@@ -0,0 +1,17 @@
+package valoeghese.shuttle-2;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import java.util.Random;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Shuttle implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("Shuttle-2");
+
+	@Override
+	public void onInitialize() {
+		// Add init code here!
+	}
+}
